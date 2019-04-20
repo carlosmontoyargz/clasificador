d1fa2bc89ce61b4ec7889e56fc7cbfcbd6c7f58d
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2019-04-19 21:03:48 -0500

    commit inicial

---
+++ src/main/java/mx/fcc/buap/md/clasificador/ClasificadorApplication.java
@@ -0,0 +1,28 @@
+package mx.fcc.buap.md.clasificador;
+
+import mx.fcc.buap.md.clasificador.storage.StorageProperties;
+import mx.fcc.buap.md.clasificador.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class ClasificadorApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ClasificadorApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ src/test/java/mx/fcc/buap/md/clasificador/ClasificadorApplicationTests.java
@@ -0,0 +1,18 @@
+package mx.fcc.buap.md.clasificador;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClasificadorApplicationTests
+{
+
+	@Test
+	public void contextLoads()
+	{
+	}
+
+}
